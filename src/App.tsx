cbcfc3d9edc4df29bf79540e018126df838d5eeb
Wenceslau <wbaneto@gmail.com>
2024-09-20 20:39:49 +0100

    feature: refactor project layout structure, add responsive components and also added drawer menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,63 @@
 import * as React from "react";
-import {ThemeProvider} from "@mui/system";
-import {Box, CssBaseline, Typography} from "@mui/material";
-import Header from "./components/Header";
+import {Box, Typography} from "@mui/material";
 import {Layout} from "./components/Layout";
-import {darkTheme, lightTheme} from "./config/tehme";
 import {Route, Routes} from "react-router-dom";
 import {CategoryList} from "./features/categories/CategoryList";
 import {CategoryEdit} from "./features/categories/CategoryEdit";
 import {CategoryCreate} from "./features/categories/CategoryCreate";
-import {SnackbarProvider} from "notistack";
-import {CastMemberList} from "./features/cast-members/CastMemberList";
-import {CastMemberEdit} from "./features/cast-members/CastMemberEdit";
 import {GenreCreate} from "./features/genre/GenreCreate";
-import {CastMemberCreate} from "./features/cast-members/CastMemberCreate";
 import {GenreEdit} from "./features/genre/GenreEdit";
 import {GenreList} from "./features/genre/GenreList";
 import {VideosList} from "./features/videos/VideosList";
 import {VideosCreate} from "./features/videos/VideosCreate";
 import {VideosEdit} from "./features/videos/VideosEdit";
+import {CastMemberEdit} from "./features/cast-members/CastMemberEdit";
+import {CastMemberCreate} from "./features/cast-members/CastMemberCreate";
+import {CastMemberList} from "./features/cast-members/CastMemberList";
 
 
 function App() {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline/>
-      <SnackbarProvider maxSnack={3} anchorOrigin={{
-        vertical: "top",
-        horizontal: "right"
-      }}>
-        {/*<Box*/}
-        {/*  component="main"*/}
-        {/*  sx={{*/}
-        {/*    height: "100vh",*/}
-        {/*    backgroundColor: (theme) => theme.palette.background.default,*/}
-        {/*  }}*/}
-        {/*>*/}
-          <Header/>
-          <Layout>
-            <Routes>
-              <Route path='/' element={<CategoryList/>}/>
-              <Route path={"/categories"} element={<CategoryList/>}/>
-              <Route path={"/categories/create"} element={<CategoryCreate/>}/>
-              <Route path={"/categories/edit/:id"} element={<CategoryEdit/>}/>
+    <div data-testid="app">
+      <Layout>
+        <Routes>
+          <Route path="/" element={<CategoryList/>}/>
 
-              <Route path={"/cast-members"} element={<CastMemberList/>}/>
-              <Route path={"/cast-members/create"} element={<CastMemberCreate/>}/>
-              <Route path={"/cast-members/edit/:id"} element={<CastMemberEdit/>}/>
+          {/* Login */}
 
-              {/* Genre */}
-              <Route path="/genres" element={<GenreList/>}/>
-              <Route path="/genres/create" element={<GenreCreate/>}/>
-              <Route path="/genres/edit/:id" element={<GenreEdit/>}/>
+          {/* Category */}
+          <Route path="/categories" element={<CategoryList/>}/>
+          <Route path="/categories/create" element={<CategoryCreate/>}/>
+          <Route path="/categories/edit/:id" element={<CategoryEdit/>}/>
 
-              {/* Videos */}
-              <Route path="/videos" element={<VideosList/>}/>
-              <Route path="/videos/create" element={<VideosCreate/>}/>
-              <Route path="/videos/edit/:id" element={<VideosEdit/>}/>
+          {/* Cast members */}
+          <Route path="/cast-members" element={<CastMemberList/>}/>
+          <Route path="/cast-members/create" element={<CastMemberCreate/>}/>
+          <Route path="/cast-members/edit/:id" element={<CastMemberEdit/>}/>
 
-              <Route path="*" element={<Typography variant="h3" component="h1">Page Not Found</Typography>}/>
-            </Routes>
+          {/* Genre */}
+          <Route path="/genres" element={<GenreList/>}/>
+          <Route
+            path="/genres/create" element={<GenreCreate/>}/>
+          <Route path="/genres/edit/:id" element={<GenreEdit/>}/>
 
-          </Layout>
-        {/*</Box>*/}
-      </SnackbarProvider>
+          {/* Videos */}
+          <Route path="/videos" element={<VideosList/>}/>
+          <Route path="/videos/create" element={<VideosCreate/>}/>
+          <Route path="/videos/edit/:id" element={<VideosEdit/>}/>
 
-    </ThemeProvider>
+          <Route path="*" element={
+            <Box sx={{color: "white"}}>
+              <Typography variant="h1">404</Typography>
+              <Typography variant="h2">Page not found</Typography>
+            </Box>
+          }
+          />
+        </Routes>
+      </Layout>
+    </div>
   );
 }
 
-
 export default App;
+
